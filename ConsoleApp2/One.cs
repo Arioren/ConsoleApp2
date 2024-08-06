60262ebead5982c55c99ac1c5618a03c2ff843af
ari oren <arioren100@gmail.com>
2024-08-06 14:11:22 +0300

    Add project files.

---
+++ ConsoleApp2/One.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static ConsoleApp2.One;
+
+namespace ConsoleApp2
+{
+    internal class One
+    {
+        public interface IPerson 
+        {
+           string  GetFullName();
+            int GetAge();
+        }
+
+        public class Person: IPerson
+        {
+            private string firstName = string.Empty;
+            private string lastName = string.Empty;
+            private DateOnly birthDate;
+
+       
+
+            public string GetFullName() {return firstName+" "+ lastName; }
+            public int GetAge() { return CalculateAge(); }
+
+            private int CalculateAge() { return DateTime.Now.Year - birthDate.Year; }
+
+        }
+
+        public class Student : Person
+        {
+            private string role = "Student";
+            public string GetRole() { return GenerateRole(); }
+            private string GenerateRole() { return role; }
+
+        }
+
+        public class Teacher : Person
+        {
+            private string role = "Teacher";
+            public string GetRole() { return GenerateRole(); }
+            private string  GenerateRole() { return role; }
+
+
+        }
+
+
+        public interface IRepository<T>
+        {
+            void Add(T item);
+
+            void remove(T item);
+            T FindById(int id);
+
+        }
+        public class User
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+        }
+        public class Product
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+        }
+        public class UserRepository : IRepository<User>
+        {
+            private List<User> users = new List<User>();
+            public void Add(User item)
+            {
+                users.Add(item);
+            }
+
+            public void remove(User item)
+            {
+                users.Remove(item);
+            }
+            public User FindById(int id)
+            {
+                return users.FirstOrDefault(u => u.Id == id);
+            }
+        }
+
+        public class ProductRepository : IRepository<Product>
+        {
+
+            private List<Product> products = new List<Product>();
+            public void Add(Product item)
+            {
+                products.Add(item);
+
+            }
+
+            public void remove(Product item)
+            {
+                products.Remove(item);
+            }
+            public Product FindById(int id)
+            {
+                return products.FirstOrDefault(p => p.Id == id);
+            }
+        }
+
+
+        public interface IMovable
+        {
+            void MoveForward();
+
+           void MoveBackward();
+           
+
+        }
+
+        public interface IFlyable
+        {
+            void TakeOff();
+            void Land();
+        }
+
+        public class FlyingCar: IFlyable, IMovable
+        {
+            private void StartEngine() { }
+
+            private void StopEngine() { }
+
+
+            public void MoveForward() { Console.WriteLine("work 5 1"); }
+
+            public void MoveBackward() { Console.WriteLine("work 5 "); }
+
+
+            public void TakeOff() { Console.WriteLine("work 5 3"); }
+            public void Land() { Console.WriteLine("work 5 4"); }
+        }
+
+
+    }
+
+
+
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,57 @@
+﻿// See https://aka.ms/new-console-template for more information
+using static ConsoleApp2.One;
+Console.WriteLine("Hello, World!");
+
+
+Teacher Teacher = new Teacher();
+Student Student = new Student();
+
+Console.WriteLine(Teacher.GetAge());
+Console.WriteLine(Teacher.GetFullName());
+Console.WriteLine(Teacher.GetRole());
+Console.WriteLine(Student.GetRole());
+
+
+
+//question 2
+
+Console.WriteLine(Student.GetRole());
+Console.WriteLine(Student.GetAge());
+//Console.WriteLine(operation2.CalculateAge()); error
+
+//question 3
+List<IPerson> persons = new List<IPerson>();
+persons.Add(Teacher);
+persons.Add(Student);
+
+foreach (IPerson person in persons)
+{
+    if (person is Student)
+    {
+        Console.WriteLine(Student.GetAge());
+    }
+    else
+    {
+        Console.WriteLine(Student.GetFullName());
+    }
+}
+
+//question 4
+UserRepository myUserRepository = new UserRepository();
+ProductRepository myProductRepository = new ProductRepository();
+
+myUserRepository.Add(new User { Id = 1, Name = "ari"});
+myProductRepository.Add(new Product { Id = 2, Name = "heli" });
+
+Console.WriteLine(myUserRepository.FindById(1));
+Console.WriteLine(myProductRepository.FindById(2));
+
+//question 5
+
+FlyingCar flyingCar = new FlyingCar();
+
+flyingCar.Land();
+flyingCar.TakeOff();
+flyingCar.MoveBackward();
+flyingCar.MoveBackward();
+
